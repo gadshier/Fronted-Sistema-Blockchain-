f8bcb097c2f2eaf13bb475bbb9265a7ab687f0ab
gadshier <gadshier23@hotmail.com>
2025-09-11 21:24:43 -0500

    Registro de Lote a nivel visual

--- src/components/LegalForm.tsx
+++ src/components/LegalForm.tsx
@@ -1,4 +1,4 @@
-import "./LegalForm.css";
+
 
 export interface LegalData {
   name: string;
@@ -14,32 +14,46 @@ interface LegalFormProps {
 
 export default function LegalForm({ data, onChange }: LegalFormProps) {
   return (
-    <div>
-      <h2 className="legal-form-title">Representante Legal</h2>
-      <input
-        className="legal-input"
+    <div className="p-6 rounded-4xl flex flex-col gap-8 items-center  shadow-md max-w-3xl  border border-blue-300 w-[350px]">
+      <h2 className="text-lg font-bold">Representante Legal</h2>
+      <div className="flex flex-col gap-4 w-full">
+        <label htmlFor="medicineName" className="lot-label">
+                Nombre completo
+        </label>
+        <input
+        className="text-gray-400 border rounded-lg px-2"
         placeholder="Nombre completo"
         value={data.name}
         onChange={(e) => onChange("name", e.target.value)}
       />
-      <input
-        className="legal-input"
+      </div>
+      <div className="flex flex-col gap-4 w-full">
+        <label className="lot-label">DNI / RUC</label>
+        <input
+        className="text-gray-400 border rounded-lg px-2"
         placeholder="Documento de identidad"
         value={data.id}
         onChange={(e) => onChange("id", e.target.value)}
       />
-      <input
-        className="legal-input"
+      </div>
+      <div className="flex flex-col gap-4 w-full">
+        <label className="lot-label">Contacto</label>
+        <input
+        className="text-gray-400 border rounded-lg px-2"
         placeholder="Teléfono"
         value={data.phone}
         onChange={(e) => onChange("phone", e.target.value)}
       />
-      <input
-        className="legal-input"
+      </div>
+      <div className="flex flex-col gap-4 w-full">
+        <label className="lot-label">N° de colegiatura</label>
+        <input
+        className="text-gray-400 border rounded-lg px-2"
         placeholder="Correo electrónico"
         value={data.email}
         onChange={(e) => onChange("email", e.target.value)}
       />
+      </div>
     </div>
   );
 }
